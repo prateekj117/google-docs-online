ffb494546ca24fae12c60c6b926caaf54b19b3ca
Prateek Jain <prateekj1171998@gmail.com>
2020-07-10 01:40:25 +0530

    Add avatar logic and basic UI for avatars

--- presence/views.py
+++ presence/views.py
@@ -1,8 +1,17 @@
+import hashlib
+
 from django.shortcuts import render, redirect
 
 
 def presence(request):
     if request.user.is_authenticated:
-        return render(request, 'presence.html')
+        avatar_url = gravatar(request.user)
+        return render(request, 'presence.html', {'avatar_url': avatar_url})
     else:
         return redirect("home")
+
+
+def gravatar(user, size=50, default='identicon', rating='g'):
+    url = 'http://www.gravatar.com/avatar'
+    hash = hashlib.md5(user.email.encode('utf-8')).hexdigest()
+    return '{url}/{hash}?s={size}&d={default}&r={rating}'.format(url=url, hash=hash, size=size, default=default, rating=rating)
