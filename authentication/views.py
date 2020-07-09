9b8d8f065507cddce6b4bb18c1720711f81fb1fd
Prateek Jain <prateekj1171998@gmail.com>
2020-07-10 00:13:55 +0530

    Add Logout functionality

--- authentication/views.py
+++ authentication/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import login, authenticate
+from django.contrib.auth import login, authenticate, logout
 
 from .forms import RegistrationForm, UserAuthenticationForm
 
@@ -50,3 +50,8 @@ def login_view(request):
 
     context['login_form'] = form
     return render(request, 'login.html', context)
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('home')
