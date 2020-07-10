5753d6ee02b43277cb232d9e25c90af1853deaf5
Prateek Jain <prateekj1171998@gmail.com>
2020-07-10 21:36:11 +0530

    Add support for .env file

--- navigus_assignment/settings.py
+++ navigus_assignment/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import environ
+
+env = environ.Env()
+
+environ.Env.read_env()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 from datetime import timedelta
@@ -22,7 +27,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'j((mis_!gr+o$!xvnwyczttu=inh%+gp&tombyz6%fyg&r64!5'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
